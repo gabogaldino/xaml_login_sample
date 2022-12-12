7fdcb919bf778a9822e83771a5ade263df7d82b4
Gabriel Galdino <gabrielgaldinogomes@gmail.com>
2022-12-11 22:25:18 -0300

    Adicionar

--- Login_Usuario/MainWindow.xaml.cs
+++ Login_Usuario/MainWindow.xaml.cs
@@ -13,12 +13,31 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-    //private readonly Context context;
-    //Usuario newProduct = new();
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        // Obter o nome de usuário e senha do usuário
+        string username = usernameTextBox.Text;
+        string password = passwordTextBox.Text;
+
+        // Verificar se o nome de usuário e senha estão corretos
+        if (username == "admin" && password == "123456")
+        {
+            // Se estiverem corretos, exibir uma mensagem de sucesso
+            MessageBox.Show("Login realizado com sucesso!");
+        }
+        else
+        {
+            // Se estiverem incorretos, exibir uma mensagem de erro
+            MessageBox.Show("Nome de usuário ou senha incorretos. Por favor, tente novamente.");
+        }
+        //private readonly Context context;
+        //Usuario newProduct = new();
 
 
-    //private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-    //{
+        //private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        //{
 
-    //}
+        //}
+    }
 }
